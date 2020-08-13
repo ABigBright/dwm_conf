2d6d0607fa9493636394b9317c72cb2d496c5486
qinmingliang <qinmingliang@cmiot.chinamobile.com>
2020-08-13 11:44:37 +0800

    fix : fix the audio cap error
    Signed-off-by: qinmingliang <qinmingliang@cmiot.chinamobile.com>

--- scripts/audio_info.sh
+++ scripts/audio_info.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 get_audio_volume() {
-    aud_vol=$(amixer get Master |tail -n1|awk '{print $4}'|sed -r 's/\[(.*)%\]/\1/g')
-    aud_mute=$(amixer get Master |tail -n1|awk '{print $6}'|sed -r 's/\[(.*)\]/\1/g')
+    aud_vol=$(amixer get Master |tail -n1|awk '{print $(NF-1)}'|sed -r 's/\[(.*)%\]/\1/g')
+    aud_mute=$(amixer get Master |tail -n1|awk '{print $(NF)}'|sed -r 's/\[(.*)\]/\1/g')
 
     if [ $aud_mute = "off" -o $aud_vol = "0" ] 
     then
